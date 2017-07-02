f460da4de4d0c94cac5c4e7ab011d4dee4bad686
Robert Kel <kel.robert@gmail.com>
2017-07-02 16:05:10 +0200

    Add composer install

--- ci/tasks/build-php.sh
+++ ci/tasks/build-php.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #Install dependencies
-#composer install --prefer-dist --no-dev --no-interaction --no-progress --no-scripts
-#composer dumpautoload -o
+composer install --prefer-dist --no-dev --no-interaction --no-progress --no-scripts
+composer dumpautoload -o
 
 cd ..
 cp -r ${PROJECT_SRC}/* built-project-php
